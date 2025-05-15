adba977916dca59cafd66c813f78f7f2823e7729
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-05-15 12:35:31 -0400

    task manager project added with a description

--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -11,6 +11,16 @@ function Work() {
                 <p>A fun and interactive web app that lets users search for and browse animated GIFs to share with friends! Built with HTML, CSS, and JavaScript, this project demonstrates how to retrieve data from an external API, fetch and process that data, and dynamically display the results in an engaging format. It’s a great example of working with APIs and asynchronous JavaScript in a real-world application.
                 Try it out and find your favorite GIF!</p>
             </div>
+            <div>
+                <a href="https://natgallagher.github.io/Gallagh-Project-3/">Task Manager App</a>
+                <p>This is a fully functional to-do list application that allows users to add, delete, mark tasks as complete, and filter tasks based on their status (all, active, or completed). Built with HTML, CSS, JavaScript, and React, the app offers a clean and responsive user interface for managing daily tasks efficiently.
+
+React is a powerful JavaScript library for building interactive user interfaces. It allows developers to build reusable components and manage application state effectively, making it easier to create fast, dynamic, and scalable web apps. In this project, React handles user interactions and dynamically updates the UI as tasks are added or changed—without reloading the page.</p>
+            </div>
+            <div>
+                <a href="https://natgallagher.github.io/Gallagh-Project-4/">Bible Verse Memory App</a>
+                <p></p>
+            </div>
         </>
     )
 }
