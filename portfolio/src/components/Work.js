97b1037c62e3abbb9522b686e1bb0103a74e95d2
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-05-15 12:16:28 -0400

    website project and description added

--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -2,6 +2,13 @@ function Work() {
     return (
         <>
             <h2>Work</h2>
+            <div>
+                <a href="https://natgallagher.github.io/Gallagh-Project-1/">Business Website</a>
+                <p>This is a clean, responsive website designed to promote the services of a freelance music arranger. Built primarily with HTML and CSS, it features an embedded audio sample player and a contact form, making it easy for potential clients to preview work and get in touch directly. This project demonstrates how creatives and small business owners can establish a professional online presence to showcase their skills and attract new opportunities.</p>
+            </div>
+            <div>
+                <a href="https://natgallagher.github.io/Gallagh-Project-2/">Giphy Search Engine</a>
+            </div>
         </>
     )
 }
