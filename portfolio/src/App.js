d53276931afa1058d59db4c889e24d9ee9e29973
NatGallagher <natgallagher@Nats-MacBook-Air.local>
2025-05-15 10:38:10 -0400

    app.js edited and moved to home.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Live Update...</p>
-      </header>
-    </div>
+    <>
+      <div>
+        <h2>Software Engineer | Crafting Innovative Software Solutions | JavaScript, React, Node.js, SQL</h2>
+        <p>Hello, I'm Nat Gallagher a Full Stack Developer</p>
+        <p>Let's Connect!</p>
+      </div>
+    </>
   );
 }
 
---
+++ portfolio/src/components/Home.js
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+      <>
+        <div>
+          <h2>Software Engineer | Crafting Innovative Software Solutions | JavaScript, React, Node.js, SQL</h2>
+          <p>Hello, I'm Nat Gallagher a Full Stack Developer</p>
+          <p>Let's Connect!</p>
+        </div>
+      </>
+    );
+  }
+  
+  export default Home;
