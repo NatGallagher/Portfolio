fdac3072f50f164832cf4bc0df8b830783620a4b
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-05-15 12:04:42 -0400

    About edited

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -2,6 +2,15 @@ function About() {
     return (
         <>
             <h2>About</h2>
+            <div>
+                <p>Hi, I’m Nat Gallagher — a certified software engineer with a passion for building practical, user-focused web applications. I earned my certification through QuickStart, in partnership with The University of Tennessee, and specialize in full stack web development.
+
+I’m proficient in a range of technologies including HTML, CSS, JavaScript, React, Node.js, and SQL. I thrive in problem-solving environments and enjoy collaborating to create clean, efficient, and scalable solutions.
+
+Before transitioning into tech, I was a teacher—an experience that sharpened my communication, collaboration, and leadership skills. That background gives me a unique edge as a developer who values clarity, teamwork, and continuous growth.
+
+I’m currently seeking opportunities as a Full Stack Developer, Front-End or Back-End Developer, or JavaScript Developer. I’m open to work and excited to contribute to a mission-driven team.</p>
+            </div>
         </>
     )
 }
