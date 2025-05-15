a3bbc2a49979b765d17faedf02c9dc7c5511e1d2
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-05-15 11:44:57 -0400

    contact us page updated

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,7 +1,29 @@
 function Contact() {
     return (
         <>
-            <h2>Contact</h2>
+            <>
+        <div>  
+          <h2>Contact Us</h2>
+          <div>
+
+          </div>
+          <br/>
+          <form>
+              <div> 
+                  <span>* First name: </span> <input type="text" maxLength={25} placeholder="* First name" required/>
+                  <br/>
+                  <span>Last name: </span> <input type="text" maxLength={25} placeholder="Last name"/>
+                  <br/>
+                  <span>* Email: </span> <input type="text" maxLength={100} placeholder="* Email" required/>
+                  <br/>
+                  <span>* Comment: </span> 
+                  <br/><textarea rows={5} cols={30} maxLength={100} placeholder="* Comment" required></textarea>
+                  <p></p>
+                  <button>Submit</button> {" "} <a>Clear</a>
+              </div>
+          </form>
+      </div>
+      </>
         </>
     )
 }
