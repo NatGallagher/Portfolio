da8f03bf462718d65464f8e950e3fdfab7aab2cc
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-05-19 14:54:51 -0400

    home.js edited

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -3,7 +3,7 @@ import profilePic from '../img/profile-pic.png';
 function Home() {
     return (
       <>
-        <div className="App" style={{backgroundColor: "#1c1c1e", color: "white"}}>
+        <div className="App" style={{backgroundColor: "#1b1b1b", color: "white"}}>
           <h2>Software Engineer | Crafting Innovative Software Solutions | JavaScript, React, Node.js, SQL</h2>
           <p>Hello, I'm Nat Gallagher <img src={profilePic} height="110" width="100" alt="Profile"></img> a Full Stack Developer</p>
           <p>Let's Connect!</p>
