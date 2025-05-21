b4a6ff6d1af5d7e1a0ac27eccf497af501237b86
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-05-21 11:12:28 -0400

    let's connect button added and linked to contact page

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,12 +1,17 @@
 import profilePic from '../img/profile-pic.png';
+import { useNavigate } from "react-router-dom"
 
 function Home() {
+
+  const navigate = useNavigate();
+
     return (
       <>
         <div className="App" style={{backgroundColor: "#1b1b1b", color: "white", minHeight: "100vh", paddingTop: "70px"}}>
           <h2 style={{paddingBottom: "70px"}}>Software Engineer | Crafting Innovative Software Solutions | JavaScript, React, Node.js, SQL</h2>
           <h4>Hello, I'm Nat Gallagher <img src={profilePic} height="110" width="100" alt="Profile" style={{borderRadius: "50%"}}></img> a Full Stack Developer</h4>
-          <button style={{
+          <button onClick={() => navigate('/contact')}
+            style={{
             backgroundColor: 'transparent',
             color: '#00d4ff',
             border: '2px solid #00d4ff',
