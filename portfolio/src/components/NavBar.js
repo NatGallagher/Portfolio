2276414666be6ed0faf01fe933c67d2af8d4466b
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-05-21 12:25:25 -0400

    home route changed to #/

--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
       <>
          <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Navbar.Brand href="#/">Home</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#/about">About</Nav.Link>
             <Nav.Link href="#/work">Work</Nav.Link>
